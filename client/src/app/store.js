17b9f40c11177b44f5a5437c4b343f9ab95b4b4d
imjord <dean.jay.goat@gmail.com>
2021-10-01 13:50:38 -0600

    added redux store file to an empty object

---
+++ client/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit' // import store api 
+
+
+// creating an empty redux store and exporting it .
+
+export default configureStore({
+    reducer: {}
+})
