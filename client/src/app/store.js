943919763c65de389dfaaa180913f400d5b731ab
imjord <dean.jay.goat@gmail.com>
2021-10-01 15:01:22 -0600

    got rid of global app crashes

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit' // import store api 
+import { reducer } from '../utils/reducers'; // my reducer to change state 
 
 
 // creating an empty redux store and exporting it .
 
-export default configureStore({
-    reducer: {}
-})
+// export default configureStore({
+//     reducer: {}
+// })
+
+const store = configureStore({ reducer });
+
+
+
+export default configureStore;
